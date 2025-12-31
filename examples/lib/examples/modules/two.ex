bf543e9a1365e90ec3249a671659be8f78736582
michaelstucki <stcuthbertus@protonmail.com>
2025-12-31 16:03:04 -0700

    modules, functions, function overloading

---
+++ examples/lib/examples/modules/two.ex
@@ -0,0 +1,21 @@
+# function overloading based on data passed in
+# not just number of arguments
+
+defmodule Examples.Modules.Two do
+  def hello do
+    "Hello"
+  end
+
+  def hello("Mike") do
+    "Hello Mike, Hello Joe"
+  end
+
+  def hello(name) do
+    "Hello #{name}"
+  end
+
+  # \\ gives fault (value to use if no argument is passed in)
+  def hi(name \\ "Reader") do
+    "Hi #{name}"
+  end
+end
