48023e66c97bf7e199065b0adbd3c6a061008768
michaelstucki <stcuthbertus@protonmail.com>
2026-01-09 14:41:05 -0700

    contained data structures, i.e., put functions that operate on a struct in the same file as the struct

---
+++ examples/lib/examples/structs/conversation.ex
@@ -0,0 +1,18 @@
+defmodule Examples.Structs.Conversation do
+  defstruct [:other_party, messages: []]
+
+  def new(sms_list) do
+    other_party = List.first(sms_list) |> Examples.Structs.Sms.other_party()
+    # %__MODULE__ returns the current module name as an atom
+    %__MODULE__{other_party: other_party, messages: sms_list}
+  end
+
+  def append(conversation, sms) do
+    new_messages = conversation.messages ++ [sms]
+    Map.put(conversation, :messages, new_messages)
+  end
+
+  def clear(consersation) do
+    Map.put(consersation, :messages, [])
+  end
+end
